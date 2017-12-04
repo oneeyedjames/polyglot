93eea9e9a42fc5fe69aeaf79c44ae8e2f141bd62
J Andrew Scott <jascott@programmer.net>
2017-12-04 16:43:51 -0500

    Wrapped modal layouts in self-contained cards

--- templates/project/form-meta.php
+++ templates/project/form-meta.php
@@ -12,23 +12,25 @@ $nonce = $this->create_nonce('save', 'project');
 ?>
 <form action="<?php echo $url; ?>" method="POST">
 	<input type="hidden" name="nonce" value="<?php echo $nonce; ?>">
-	<header>Edit Project</header>
+	<div class="modal card blue" id="modal-form-meta">
+		<header>Edit Project</header>
 
-	<label class="control-label">Title</label>
-	<input type="text" name="project[title]" value="<?php echo $project->title; ?>" class="form-control">
+		<label class="control-label">Title</label>
+		<input type="text" name="project[title]" value="<?php echo $project->title; ?>" class="form-control">
 
-	<label class="control-label">Description</label>
-	<textarea name="project[description]" class="form-control"><?php echo $project->descrip; ?></textarea>
+		<label class="control-label">Description</label>
+		<textarea name="project[description]" class="form-control"><?php echo $project->descrip; ?></textarea>
 
-	<label class="control-label">Default Language</label>
-	<select name="project[language]" class="form-control">
-		<?php foreach ($languages as $language) : ?>
-			<option value="<?php echo $language->id; ?>"<?php echo $language->id == $project->default_language_id ? ' selected="selected"' : ''; ?>><?php echo $language->name; ?></option>
-		<?php endforeach; ?>
-	</select>
+		<label class="control-label">Default Language</label>
+		<select name="project[language]" class="form-control">
+			<?php foreach ($languages as $language) : ?>
+				<option value="<?php echo $language->id; ?>"<?php echo $language->id == $project->default_language_id ? ' selected="selected"' : ''; ?>><?php echo $language->name; ?></option>
+			<?php endforeach; ?>
+		</select>
 
-	<footer>
-		<button type="button" class="btn cancel">Cancel</button>
-		<button type="submit" class="btn blue">Save</button>
-	</footer>
+		<footer>
+			<button type="button" class="btn cancel">Cancel</button>
+			<button type="submit" class="btn blue">Save</button>
+		</footer>
+	</div>
 </form>
--- templates/project/modal-documents.php
+++ templates/project/modal-documents.php
@@ -1,17 +1,19 @@
-<header>
-    <i class="fa fa-folder-open"></i> <?php echo $project->title; ?>
-    <a href="#" class="cancel pull-right"><i class="fa fa-close"></i></a>
-</header>
-<strong><i class="fa fa-file"></i> Documents</strong>
-<ul>
-    <?php foreach ($project->documents as $document) : ?>
-        <li>
-            <a href="document/<?php echo $document->id; ?>">
-                <?php echo $document->title; ?>
-            </a>
-        </li>
-    <?php endforeach; ?>
-    <?php if ($project->documents->found > count($project->documents)) : ?>
-        <li><em>more</em></li>
-    <?php endif; ?>
-</ul>
+<div class="modal card cyan" id="modal-card-documents">
+    <header>
+        <i class="fa fa-folder-open"></i> <?php echo $project->title; ?>
+        <a href="#" class="cancel pull-right"><i class="fa fa-close"></i></a>
+    </header>
+    <strong><i class="fa fa-file"></i> Documents</strong>
+    <ul>
+        <?php foreach ($project->documents as $document) : ?>
+            <li>
+                <a href="document/<?php echo $document->id; ?>">
+                    <?php echo $document->title; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+        <?php if ($project->documents->found > count($project->documents)) : ?>
+            <li><em>more</em></li>
+        <?php endif; ?>
+    </ul>
+</div>
