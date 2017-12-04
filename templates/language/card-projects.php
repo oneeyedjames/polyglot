b960ed0a575276f9679b4e167591e65098148d2e
J Andrew Scott <jascott@programmer.net>
2017-12-04 16:33:33 -0500

    Wrapped modal layouts in self-contained cards

--- templates/language/card-projects.php
+++ templates/language/card-projects.php
@@ -1,19 +1,19 @@
-<header>Projects</header>
-<h4><i class="fa fa-flag"></i> <?php echo $language->name; ?></h4>
-<?php $label = ' Project' . ($language->projects->found > 1 ? 's' : ''); ?>
-<strong><?php echo $language->projects->found . $label; ?></strong>
-<ul>
-    <?php foreach ($language->projects as $project) : ?>
-        <li>
-            <a href="project/<?php echo $project->id; ?>">
-                <?php echo $project->title; ?>
-            </a>
-        </li>
-    <?php endforeach; ?>
-    <?php if ($language->projects->found > count($language->projects)) : ?>
-        <li><em>more</em></li>
-    <?php endif; ?>
-</ul>
-<footer>
-    <button class="btn cancel">Close</button>
-</footer>
+<div class="modal card cyan" id="modal-card-projects">
+    <header>
+        <i class="fa fa-flag"></i> <?php echo $language->name; ?>
+        <a class="pull-right cancel"><i class="fa fa-close"></i></a>
+    </header>
+    <strong><i class="fa fa-folder-open"></i> Projects</strong>
+    <ul>
+        <?php foreach ($language->projects as $project) : ?>
+            <li>
+                <a href="project/<?php echo $project->id; ?>">
+                    <?php echo $project->title; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+        <?php if ($language->projects->found > count($language->projects)) : ?>
+            <li><em>more</em></li>
+        <?php endif; ?>
+    </ul>
+</div>
