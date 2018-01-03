c8439f13f81905131ee0e07283ce50eeeac65f2e
J Andrew Scott <jascott@programmer.net>
2018-01-03 14:32:04 -0500

    Added alias to user record

--- assets/sql/user-schema.sql
+++ assets/sql/user-schema.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS `user` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    `password` varchar(128) NOT NULL,
+    `alias` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
     `admin` tinyint(3) unsigned NOT NULL DEFAULT 0,
+    `password` varchar(128) NOT NULL,
     `reset_token` varchar (255) NULL,
 	`reset_expire` datetime NULL,
     PRIMARY KEY (`id`),
+    UNIQUE KEY `alias` (`alias`),
     UNIQUE KEY `email` (`email`)
 ) DEFAULT CHARSET=utf8;
