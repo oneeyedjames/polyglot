853dce45253556c18aa14a383c84eb04c6e19ed3
J Andrew Scott <jascott@programmer.net>
2018-07-23 16:07:14 -0400

    Updated dashboard view

--- templates/index.php
+++ templates/index.php
@@ -3,39 +3,21 @@
 </ol>
 <div class="row">
 	<div class="col-md-4 col-lg-3">
-		<div class="card info">
+		<div class="card info collapsible">
 			<header><i class="fa fa-folder-open"></i> My Projects</header>
 			<ul>
 				<li><a href="#">Project Name</a></li>
 			</ul>
-			<div class="align-right">
+			<!-- <div class="align-right">
 				<a href="projects" class="btn sm text primary">
 					See More <i class="fa fa-chevron-right"></i>
 				</a>
-			</div>
+			</div> -->
 		</div>
-		<div class="card info">
-			<header><i class="fa fa-file"></i> My Documents</header>
+		<div class="card info collapsible">
+			<header><i class="fa fa-flag"></i> My Languages</header>
 			<ul>
-				<li>
-					<a href="#">Document</a>
-					<div>
-						<i class="fa fa-folder-open"></i>
-						in <a href="project/1">Project</a>
-					</div>
-				</li>
-			</ul>
-		</div>
-		<div class="card info">
-			<header><i class="fa fa-list"></i> My Term Lists</header>
-			<ul>
-				<li>
-					<a href="#">Term List</a>
-					<div>
-						<i class="fa fa-folder-open"></i>
-						in <a href="project/1">Project</a>
-					</div>
-				</li>
+				<li>Language</li>
 			</ul>
 		</div>
 	</div>
